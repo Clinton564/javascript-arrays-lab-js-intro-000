58bbc33ac2345ba27cf47ade3aaa864f24cba290
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-10-20 22:52:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,33 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 function destructivelyAppendKitten(name) {
   kittens.push(name)
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.shift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.unshift()
+}
+
+function appendKitten(name) {
+  var array = [...kittens, name]
+  return array
+}
+
+function prependKitten(name) {
+  var array = [name, ...kittens]
+  return array
+}
+
+function removeLastKitten() {
+  return kittens.slice(1, kittens.length - 1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(2,kittens.length)
+}
